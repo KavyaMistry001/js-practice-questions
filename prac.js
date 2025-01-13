9ac43624b0e4544eb7af3c9ca6ef620b7c531081
Kavya Mistry <157121447+KavyaMistry001@users.noreply.github.com>
2025-01-13 18:34:40 +0530

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,10 @@
+function EvenOrOdd(number) {
+    if (number % 2 === 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
+  console.log(EvenOrOdd(2)); 
+  console.log(EvenOrOdd(3)); 
+  
