b75c12626a58c7d1620acb33711a5f4fab8df8b7
Kavya Mistry <157121447+KavyaMistry001@users.noreply.github.com>
2025-01-15 16:59:32 +0530

    Add files via upload

---
+++ prac4.js
@@ -0,0 +1,11 @@
+function checkpalindrome(word) {
+    let reversedWord = word.split("").reverse().join("");
+  
+    if (word === reversedWord) {
+      console.log(word + " is a palindrome!");
+    } else {
+      console.log(word + " is not a palindrome.");
+    }
+  }
+  checkpalindrome("madam"); 
+  checkpalindrome("hello"); 
