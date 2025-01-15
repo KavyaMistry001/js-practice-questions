1c58ddfdeb2a9e9e593c7c1a73fca8dc429b5b94
Kavya Mistry <157121447+KavyaMistry001@users.noreply.github.com>
2025-01-15 17:39:15 +0530

    prac4.js

--- prac4.js
+++ prac4.js
@@ -2,7 +2,7 @@ function checkpalindrome(word) {
     let reversedWord = word.split("").reverse().join("");
   
     if (word === reversedWord) {
-      console.log(word + " is a palindrome!");
+      console.log(word + " is a palindrome");
     } else {
       console.log(word + " is not a palindrome.");
     }
