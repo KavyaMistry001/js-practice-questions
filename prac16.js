3286fc93a2b3db7d0356cd45bb1b81fe1e8a2a9f
Kavya Mistry <157121447+KavyaMistry001@users.noreply.github.com>
2025-01-15 19:04:04 +0530

    Add files via upload

---
+++ prac16.js
@@ -0,0 +1,8 @@
+function delay(ms) {
+    setTimeout(function() {
+      console.log("Delay of " + ms + " milliseconds is over");}, ms);
+  }
+  
+   delay(2000);
+   console.log("Delay is happening");
+  
